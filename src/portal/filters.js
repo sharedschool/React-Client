c25b5b2661480fc76b9cbbbe201dae6d066872a4
peterapps <peterapps2016@gmail.com>
2019-07-29 17:13:54 -0400

    Finished Browse landing page

---
+++ src/portal/filters.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Form } from "react-bootstrap";
+
+export class Price extends React.Component {
+	render(){
+		return (
+			<Form.Group controlId="price">
+				<Form.Control
+					as="select"
+					value={this.props.value}
+					onChange={this.props.callback}
+				>
+					<option value="any">Any price</option>
+					<option value="0">$0 to $99</option>
+					<option value="100">$100 to $499</option>
+					<option value="500">$500 to $999</option>
+					<option value="1000">$1000 or greater</option>
+				</Form.Control>
+			</Form.Group>
+		);
+	}
+}
+
+export class Location extends React.Component {
+	render(){
+		return (
+			<Form.Group controlId="location">
+				<Form.Control
+					as="select"
+					value={this.props.value}
+					onChange={this.props.callback}
+				>
+					<option value="any">Any distance</option>
+					<option value="10">Within 10 miles</option>
+					<option value="50">Within 50 miles</option>
+					<option value="100">Within 100 miles</option>
+				</Form.Control>
+			</Form.Group>
+		);
+	}
+}
+
+export class Subject extends React.Component {
+	render(){
+		return (
+			<Form.Group controlId="subject">
+				<Form.Control
+					as="select"
+					value={this.props.value}
+					onChange={this.props.callback}
+				>
+					<option value="any">Any subject</option>
+					<option>Math</option>
+					<option>Science</option>
+					<option>English</option>
+					<option>History</option>
+					<option>Art</option>
+				</Form.Control>
+			</Form.Group>
+		);
+	}
+}
+
+export class Institution extends React.Component {
+	render(){
+		return (
+			<Form.Group controlId="institution">
+				<Form.Control
+					as="select"
+					value={this.props.value}
+					onChange={this.props.callback}
+				>
+					<option value="any">Any institution</option>
+					<option>Public</option>
+					<option>Private</option>
+				</Form.Control>
+			</Form.Group>
+		);
+	}
+}
